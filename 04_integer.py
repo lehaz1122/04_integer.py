48f381ac8b8a1499f9417c6ef0f438cd5a98ee14
lehaz1122 <lehadkhan1122@gmail.com>
2025-08-16 11:25:50 +0500

    Add files via upload

---
+++ 04_integer.py
@@ -0,0 +1,39 @@
+#integer (int) : whole number (+, -) with no decimal point age = 15
+
+student_name = "Lehaz Khan"
+student_age = 23
+
+print(f"hi {student_name}, your age is {student_age}")
+
+student_age = 23
+print(student_age)
+
+student_age2 = "23"
+print(student_age2)
+
+checker = student_age == student_age2
+print(checker)
+
+print(type(student_age))
+
+print(type(student_age2))
+
+first_name = "Lehaz"
+last_name = "Khan"
+
+print(first_name + last_name)
+
+print(1 + 1)
+print("1" + "1")
+
+age = int(input("Enter Your Age: "))
+age2 = int(input("Enter your age 2: ")) 
+
+print(age + age2)
+
+student_name="Lehaz"
+student_age=23
+
+print(f"hi{student_name},your age is{student_age}")
+
+
